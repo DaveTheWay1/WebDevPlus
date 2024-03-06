b091bd35665c9bfb5b1d18c5f45c973a19b997f4
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-03-06 08:33:45 -0800

    accessing/updating elements, creating arrays, removing elements, splice

---
+++ javaScript/arrays/creatingArray.js
@@ -0,0 +1,11 @@
+// * Two Ways toe create arrays
+
+// using Array Literal Notation (recommended best practice)
+const nums1 = [2, 4, 18];
+
+// using the Array class (less common approach)
+const nums2 = new Array(2, 4, 18);
+
+// The best practice is to use Array Literal Notation because 
+// it’s more concise and the class approach behaves differently 
+// if you pass only one argument.
