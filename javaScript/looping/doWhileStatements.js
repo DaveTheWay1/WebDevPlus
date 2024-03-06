f03fb26427ead0b8d75e3965166030c1cab43366
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-03-06 12:51:56 -0800

    do while statements

---
+++ javaScript/looping/doWhileStatements.js
@@ -0,0 +1,15 @@
+// * do…while
+// You may choose to use the do...while statement 
+// instead of while to force the code block 
+// to always execute at least once.
+let num = 0;
+do {
+    console.log(num + ' is even');
+    num += 2;
+} while (num <= 10);
+// The do-while loop is a type of loop in programming 
+// that executes a block of code once before checking 
+// if the condition is true, and then it will repeat 
+// the loop as long as the condition remains true.
+
+// * so the do always happens for every loop in the while as long as it is true.
