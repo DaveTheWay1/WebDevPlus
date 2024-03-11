44e425c2b883ba9b74f006457ddcce5953d7edbe
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-03-11 15:55:26 -0700

    accidental change somewhere but code still works fine

--- javaScript/DOM/DOM-Menu-Lab/script.js
+++ javaScript/DOM/DOM-Menu-Lab/script.js
@@ -17,33 +17,18 @@ const menuLinks = [
     ]},
 ];
 
-  // Task 1.0
 const mainEl = document.querySelector('main');
 
-  // Task 1.1
 mainEl.style.backgroundColor = 'var(--main-bg)';
-
-  // Task 1.2
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
-
-  // Task 1.3
 mainEl.classList.add('flex-ctr');
 
-  // Task 2.0
 const topMenuEl = document.getElementById('top-menu');
-  // Task 2.1
+
 topMenuEl.style.height = '100%'
-
-  // Task 2.2
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
-
-  // Task 2.3
 topMenuEl.classList.add('flex-around');
 
-  // Task 3.0
-  // menuLinks data structure copied at the top
-
-  // Task 3.1
 menuLinks.forEach(function(link) {
     const linkEl = document.createElement('a');
     linkEl.setAttribute('href', link.href);
@@ -51,56 +36,38 @@ menuLinks.forEach(function(link) {
     topMenuEl.appendChild(linkEl);
 });
 
-  // Task 4.0
 const subMenuEl = document.getElementById('sub-menu');
 
-  // Task 4.1
 subMenuEl.style.height = '100%';
-
-  // Task 4.2
 subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)';
-
-  // Task 4.3
 subMenuEl.classList.add('flex-around');
 
-  // Task 4.4
 subMenuEl.style.position = 'absolute';
-  // Task 4.5
 subMenuEl.style.top = '0';
 
-  // Task 5.0
-  // menuLinks array updated at the top
 
-  // Task 5.1
 const topMenuLinks = document.querySelectorAll('#top-menu a');
 let showingSubMenu = false;
 
-  // Task 5.2
 topMenuEl.addEventListener('click', function(evt) {
     evt.preventDefault();
     const link = evt.target;
     if (link.tagName !== 'A') return;
     console.log(link.textContent);
-    // Task 5.3
     if (link.classList.contains('active')) {
         link.classList.remove('active');
         showingSubMenu = false;
         subMenuEl.style.top = '0';
         return;
     }
-    // Task 5.4
     topMenuLinks.forEach(function(link) {
         link.classList.remove('active');
     });
-    // Task 5.5
     link.classList.add('active');
-    // Task 5.6
     const linkData = menuLinks.find(function(linkObj) {
         return linkObj.text === link.textContent;
     });
     showingSubMenu = 'subLinks' in linkData;
-    // Task 6.4
-    // Task 5.7
     if (showingSubMenu) {
         buildSubMenu(linkData.subLinks);
         subMenuEl.style.top = '100%';
@@ -110,7 +77,6 @@ topMenuEl.addEventListener('click', function(evt) {
     }
 });
 
-  // Task 5.8
 function buildSubMenu(subLinks) {
     subMenuEl.innerHTML = '';
     subLinks.forEach(function(link) {
@@ -121,7 +87,6 @@ function buildSubMenu(subLinks) {
     });
 }
 
-  // Task 6.0
 subMenuEl.addEventListener('click', function(evt) {
     evt.preventDefault();
     const link = evt.target;
@@ -134,7 +99,6 @@ subMenuEl.addEventListener('click', function(evt) {
     topMenuLinks.forEach(function(link) {
         link.classList.remove('active');
     });
-    // Task 6.3
     mainEl.innerHTML = `<h1>${link.textContent}</h1>`;
 });
 
