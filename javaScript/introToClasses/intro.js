2ce3197c08a77f9eaefef0485200ef881959e629
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-03-12 12:39:10 -0700

    class in js, overriding, instance methods

--- javaScript/introToClasses/intro.js
+++ javaScript/introToClasses/intro.js
@@ -40,14 +40,53 @@ class Vehicle{
     // * constructor method used to recieve inputs
     constructor(make,model,color){
         // * inputs are used to assign values to the properties for the resulting objects
+        // * you dont always have to pass everything as parameter, you can have a default
         // ? how are the properties and their corresponding values attatched?
         this.make = make;
         this.model = model;
         this.color = color;
+        this.isRunning = false;
         // we use the "this" keyword
         // * this refers to the new object we create.
     }
 }
 
+const car3 = new Vehicle('dodge','charger', 'maroon');
+console.log(car3.isRunning);
+// output: false
 const car1 = new Vehicle('ford','mustang', 'red');
+console.log(car1.isRunning);
+// output: false
+
+console.log(car1.model);
+// * "this"
+// refers to the object that the class creates even in the process of constructing it.
+// the car1 is the same as the "this" -> they both point/refer to the same thing
+
 const car2 = new Vehicle('honda', 'accord', 'grey');
+// * the new keyword signals to javascript that we are about to create a new object
+// javascript will look at the args and then at the constructor and assign as properties.
+
+
+console.log(typeof Vehicle);
+// output: function
+// ? why? 
+// * prior to 2015 javascript did not have classes
+
+
+// ? ❓ What other attributes might a car have?
+// const hybridCar = {
+//     make: 'Toyota',
+//     model: 'Prius',
+//     color: 'black',
+//     isRunning: false,
+//     start: function() {
+//         hybridCar.isRunning = true;
+//         console.log('Running!')
+//     },
+// };
+
+// * classes vocab
+// instance: an object created by a class
+// instantiate:  
+// instantiation:  
