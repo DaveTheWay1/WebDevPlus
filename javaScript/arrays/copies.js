5ecbaba251e5e75ca1dec14fc282cbf28b1f33d6
David Ramirez Vazquez <ramirezvazquez826@gmail.com>
2024-03-24 12:39:23 -0700

    pushing to github

--- javaScript/arrays/copies.js
+++
@@ -1,9 +0,0 @@
-// * Copy all of an array
-let movies = ['Best In Show', 'Alien', 'The Matrix', 'Gladiator']
-const moviesCopy = [...movies];
-// moviesCopy //-> ['Best In Show', 'Alien', 'The Matrix', 'Gladiator']
-
-// * Copy All of an Array & Insert Additional Elements
-twoMovies //-> ['Alien', 'The Matrix']
-const fourMovies = [ 'Amadeus', ...twoMovies, 'The Sting' ];
-fourMovies //-> ['Amadeus', 'Alien', 'The Matrix', 'The Sting'];
--- javaScript/dataTypes/null.js
+++
@@ -1,9 +0,0 @@
-// * null
-// * The null data type has only one value: null
-// We often assign the value null to a variable to represent the fact that it has no “real” value 
-
-ex:
-null;
-let winner = null;
-winner === null;
-// ^ true
